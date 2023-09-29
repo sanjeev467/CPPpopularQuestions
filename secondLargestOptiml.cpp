caac167f267745a8e1363d3243e129e8e4c5458f
Sanjeev Pandey <84841764+sanjeev467@users.noreply.github.com>
2023-09-29 14:50:30 +0530

    secondLargestOptimal

---
+++ secondLargestOptiml.cpp
@@ -0,0 +1,58 @@
+// #include <iostream>
+#include <bits/stdc++.h>
+
+int main()
+{
+
+    int n;
+    std::cout << "Enter Your No: ";
+    std::cin >> n;
+    std::cout << "You Entered: " << n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Elements in Array are: " << arr[i] << std::endl;
+    }
+
+    int largest = arr[0];
+    int smallest = arr[0];
+    int secongLargest = INT_MIN;
+    int secondSmallest = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            secongLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secongLargest && arr[i] != largest)
+        {
+            secongLargest = arr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] < secondSmallest && arr[i] != smallest)
+        {
+            secondSmallest = arr[i];
+        }
+    }
+
+    std::cout << secongLargest << std::endl;
+    std::cout << secondSmallest << std::endl;
+
+    return 0;
+}
