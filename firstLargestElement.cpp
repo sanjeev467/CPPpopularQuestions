a967613c29b9060f0f6564d0519e5dd99be50134
Sanjeev Pandey <84841764+sanjeev467@users.noreply.github.com>
2023-09-29 13:05:51 +0530

    Array Examples

---
+++ firstLargestElement.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+    int n;
+    std::cout << "Enter Your No: ";
+    std::cin >> n;
+    std::cout << "You Entered: " << n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Elements in Array are: " << arr[i] << std::endl;
+    }
+    int max = arr[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[0])
+        {
+            max = arr[i];
+        }
+    }
+    std::cout << max;
+
+    return 0;
+}
