bb39a47f0f8e96158fdc1462142f2105fbe4350e
Sanjeev Pandey <84841764+sanjeev467@users.noreply.github.com>
2023-11-01 11:33:33 +0530

    Example added

---
+++ linearSearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    cout << endl;
+
+    int key = 4;
+    5
+
+        for (int i = 0; i < n; i++)
+    {
+        if (key == arr[i])
+        {
+            cout << arr[i];
+        }
+    }
+    return 0;
+}
